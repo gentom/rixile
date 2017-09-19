4150c60c4ebcf7fa08eccd125d4477adb9ccea56
gentom <gento.m.developer@gmail.com>
2017-09-19 12:52:15 +0900

    commit

--- lib/rixile.ex
+++ lib/rixile.ex
@@ -15,4 +15,15 @@ defmodule Rixile do
   def hello do
     :world
   end
+
+  def init(default_opts) do
+    IO.puts "starting up Helloplug..."
+    default_opts
+  end
+
+  def call(conn, _opts) do
+    IO.puts "saying hello!"
+    Plug.Conn.send_resp(conn, 200, "Hello, world!")
+  end
+  
 end
