23f0da6ab2494a3e614603791e447b18e5cd34bb
gentom <gento.m.developer@gmail.com>
2017-09-19 20:12:50 +0900

    commit

--- lib/rixile.ex
+++ lib/rixile.ex
@@ -24,6 +24,27 @@ defmodule Rixile do
   def call(conn, _opts) do
     IO.puts "saying hello!"
     conn |> Plug.Conn.put_resp_header("Server", "Plug") |> Plug.Conn.send_resp(200, "Hello, world!")
+    #conn2 = Plug.Conn.put_resp_header(conn, "Server", "Plug")
+    #Plug.Conn.send_resp(conn2, 200, "Hello, world!")
+  end
+
+  def call(conn, _opts) do
+    route(conn.method, conn.path_info, conn)
+  end
+
+  def route("GET", ["hello"], conn) do
+    # this route is for /hello
+    conn |> Plug.Conn.send_resp(200, "Hello, world!")
+  end
+
+  def route("GET", ["users", user_id], conn) do
+    # this route is for /users/<user_id>
+    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+  end
+
+  def route(_method, _path, conn) do
+    # this route is called if no other routes match
+    conn |> Plug.Conn.send_resp(404, "Couldn't find that page, sorry!")
   end
 
 end
