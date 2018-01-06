4e22c032245a5b224f5c74ec49a19c72fa083475
gentom <gento.m.developer@gmail.com>
2018-01-06 10:48:19 +0900

    commit

---
+++ lib/rixile.ex
@@ -0,0 +1,18 @@
+defmodule Rixile do
+  @moduledoc """
+  Documentation for Rixile.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rixile.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rixile_test.exs
@@ -0,0 +1,8 @@
+defmodule RixileTest do
+  use ExUnit.Case
+  doctest Rixile
+
+  test "greets the world" do
+    assert Rixile.hello() == :world
+  end
+end
