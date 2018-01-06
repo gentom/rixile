70bc9fc2886f75c6596aa424145557ad576007fd
gentom <gento.m.developer@gmail.com>
2018-01-06 11:11:53 +0900

    update call func

--- lib/rixile.ex
+++ lib/rixile.ex
@@ -21,9 +21,22 @@ defmodule Rixile do
     default_opts
   end
 
+  '''
   def call(conn, _opts) do
     IO.puts "Calling Rixile call"
     Plug.Conn.send_resp(conn, 200, "Hello Rixile :)")
+  end
+  '''
+
+  def call(conn, _opts) do
+    IO.puts "Calling Rixile call"
+    # `A() |> B() |> C()` means `C(B(A))`
+    conn |> Plug.Conn.put_resp_header("Server", "Plug") |> Plug.Conn.send_resp(200, "Hello Rixile :)")
+    # you can rewrite the line above
+    '''
+    conn2 = Plug.Conn.put_resp_header(conn, "Server", "Plug")
+    Plug.Conn.send_resp(conn2, 200, "Hello Rixile :)")
+    '''
   end
 
 end
