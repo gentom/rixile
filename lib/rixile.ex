4554ca81291352aff5e5bf663a3b417ebb3eccdc
gentom <gento.m.developer@gmail.com>
2018-01-06 11:04:09 +0900

    commit to rixile.ex

--- lib/rixile.ex
+++ lib/rixile.ex
@@ -15,4 +15,15 @@ defmodule Rixile do
   def hello do
     :world
   end
+
+  def init(default_opts) do
+    IO.puts "starting up Rixile..."
+    default_opts
+  end
+
+  def call(conn, _opts) do
+    IO.puts "Calling Rixile call"
+    Plug.Conn.send_resp(conn, 200, "Hello Rixile :)")
+  end
+
 end
